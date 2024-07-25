50deeae86c2b3a198d14a2663b44e07a9c8ae259
screamt-3 <103256665+screamt-3@users.noreply.github.com>
2024-07-25 22:59:26 +0800

    Update Home.jsx

--- Login_Register/client/src/Home.jsx
+++ Login_Register/client/src/Home.jsx
@@ -7,22 +7,22 @@ import './color.css';
 
 function Home() {
     return (
-        <div>
-            <h1 className='color-blue-royal'>Welcome to Ez-Rent's Homepage</h1>
+        <div className='bg-blue-royal'>
+            <h1 className='d-flex justify-content-center'>
+                Ez-Rent Homepage
+            </h1>
+            <div className='d-flex justify-content-center align-items-center'>
+            <Link to="/CreateRental" className="btn btn-primary">
+                Add Rental
+            </Link>
+            </div>
             <div>
-
+                <SearchBar />
             </div>
-            <div style={{height: '200%'}} className="d-flex justify-content-end align-items-center bg-white vh-100">
-                <div className="bg-blue-royal p-3 rounded w-25">
-                    <Link to="/CreateRental" className="btn btn-default w-100 bg-white rounded-1 text-decoration-none color-blue-royal">
-                        Add Rental
-                    </Link>
 
-                </div>
-            </div>
         </div>
 
-            )
+    )
 }
 
 export default Home;
