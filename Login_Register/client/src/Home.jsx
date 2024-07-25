2004764b0c05338f14151bbb2baa9a85febba844
screamt-3 <103256665+screamt-3@users.noreply.github.com>
2024-07-25 23:19:48 +0800

    Update Home.jsx

--- Login_Register/client/src/Home.jsx
+++ Login_Register/client/src/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
 import './color.css';
+import { SearchBar } from './SearchBar';
 
 function Home() {
     return (
